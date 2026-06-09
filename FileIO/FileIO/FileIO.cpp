cb184d912c6ac64e76557ae826521c3b34d015ef
Boone <fboonestewart05@gmail.com>
2026-06-08 18:55:58 -0700

    started new file

---
+++ FileIO/FileIO/FileIO.cpp
@@ -0,0 +1,22 @@
+// FileIO.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+
+}
+
+/*
+Your app should have a good UX.
+Over comment your code!
+Display a description of the program and how to use it for the user when your app starts.
+Your app must be able to write the same contents to a text file and the screen at the same time.
+Your app must be able to read the data from a text file and then write the contents to the display.
+*/
