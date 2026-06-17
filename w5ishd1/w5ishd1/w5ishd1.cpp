8fccc66544f0449ed8a5e68273923f4fdcc828c8
Boone <fboonestewart05@gmail.com>
2026-06-16 17:01:46 -0700

    started pointer project

--- w5ishd1/w5ishd1/w5ishd1.cpp
+++ w5ishd1/w5ishd1/w5ishd1.cpp
@@ -5,22 +5,117 @@
 #include <fstream>
 #include <string>
 
-using namespace std;
+
+//I was going to do a calculator but then I realized that all my functions were just taking doubles
+
+class fencer {
+public:
+	std::string name = "doe";
+
+	int score = 0;
+
+	int wins = 0;
+
+	fencer(std::string n, int s) {
+		name = n;
+		score = s;
+	}
+};
+
+void makeBracket(fencer* fencers) {
+	
+}
+
+void listFencers(fencer* fencers) {
+	
+	std::cout << "The fencers are: " << std::endl;
+	for (int i = 0; i < (sizeof(fencers)); i++) {
+		std::cout << fencers[i].name << std::endl;
+	}
+}
+
+
+void countRounds(fencer* one, fencer* two, int* length) {
+	
+	std::cout << "counting rounds" << std::endl;
+
+	while (true){
+		std::string winner;
+		std::cout << "Who's point?" << std::endl;
+		std::cout << "1 | " << one->name << std::endl;
+		std::cout << "2 | " << two->name << std::endl;
+		std::cout << "3 | Double"  << std::endl;
+		std::cout << "4 | No point" << std::endl;
+
+		std::cin >> winner;
+
+		if (winner == "1") {
+			one->score += 1;
+		}
+		else if (winner == "2") {
+			two->score += 1;
+		}
+		else if (winner == "3") {
+			one->score += 1;
+			two->score += 1;
+		}
+		else {
+			continue;
+		}
+
+		if (one->score >= *length || two->score >= *length) {
+			if (one->score - two->score > 1) {
+				std::cout << one->name << " wins!" << std::endl;
+				one->wins += 1;
+				break;
+			}
+			else if (two->score - one->score > 1) {
+				std::cout << one->name << " wins!" << std::endl;
+				two->wins += 1;
+				break;
+			}
+		}
+
+		std::cout << "Current scores: " << std::endl;
+		std::cout << one->name << ": " << one->score << "/" << *length << std::endl;
+		std::cout << two->name << ": " << two->score << "/" << *length << std::endl;
+	}
+}
+
+void displayBracket() {
+
+}
+
+void clearBracket() {
+
+}
 
 int main()
 {
-    // &variable is the memory address of a variable
-    //datatype* name is declaring a pointer variable that stores the address for a given datatype
-    //datatype* ptr = &variable creates a pointer called ptr that stores the memory address of variable. 
-    // printing ptr will print the long address
-    // printing *ptr will print the value of the variable who's address is being stored in the pointer
-    // *ptr = 100 will change the value of the original number
+	fencer stemble = fencer("Stemble", 0);
 
-    int* ptr = new int;
-    *ptr = 75;
-    cout << *ptr;
-    delete ptr; //I don't know what new and delete do
+	fencer mssck = fencer("Mssck", 0);
 
+	fencer grrlo = fencer("Grrlo", 0);
+
+	fencer bandin = fencer("Bandin", 0);
+
+	fencer clwon = fencer("Clwon", 0);
+
+	fencer zlack = fencer("Zlack", 0);
+
+	fencer fencers[6] = {stemble, mssck, grrlo, bandin, clwon, zlack};
+
+	listFencers(fencers);
+
+	std::cout << "This line runs" << "\n";
+	int roundNorm = 10;
+
+	makeBracket(fencers);
+
+	listFencers(fencers);
+
+	//countRounds(&stemble, &mssck, &roundNorm);
 }
 /*
 Create a new application of your choice.
